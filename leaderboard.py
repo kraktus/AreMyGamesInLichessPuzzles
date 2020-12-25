94af52da1a97cdc63c9781b3f8c440c0edcf2e22
kraktus <56031107+kraktus@users.noreply.github.com>
2020-12-25 14:11:22 +0000

    Publish leaderboard for latest puzzle db, finish code refractoring
    17 games are not taken in count, see https://github.com/ornicar/lila/issues/7786

--- leaderboard.py
+++ leaderboard.py
@@ -62,11 +62,15 @@ class Downloader:
             if game_to_puzzle_id.pop(game_id, None) is None:
                 print(f"game {game_id} was dl but not in the puzzle db, Error")
         # Only games in the db not dl are left in the dic
+        print(game_to_puzzle_id)
         games_not_dl = list(game_to_puzzle_id.keys())
         print(f"{len(games_not_dl)} games left to be dl, expecting {(self.tl() + len(games_not_dl)/20):.2f}s")
         with open(GAMES_DL_PATH, "a") as output:
             for i in range(0, len(games_not_dl), 300): #dl games 300 at a time
+                print(i)
+                print(games_not_dl[i:i+300])
                 res = self.req(games_not_dl[i:i+300])
+                print(res)
                 output.write(res)
 
     def req(self, games: List[str]) -> str:
@@ -123,7 +127,7 @@ class FileHandler:
 
     def compute(self) -> List[Row]:
         dic = {}
-        game_to_puzzle_id = game_puzzle_id()
+        game_to_puzzle_id = self.game_puzzle_id()
         with open(GAMES_DL_PATH, "r") as file_input:
             for line in file_input:
                 args = line.split() # game id, white player, black player
@@ -157,15 +161,21 @@ class FileHandler:
         Check if a game is present twice or more
         """
         s = set()
+        sanity = True
         print("checking sanity of the games dl...")
         with open(GAMES_DL_PATH, "r") as file_input:
             for line in file_input:
                 game_id = line.split()[0]
                 if game_id in s:
                     print(game_id)
+                    sanity = False
                 else:
                     s.add(game_id)
 
+        if sanity:
+            print("games dl are sane")
+        else:
+            raise Exception("games dl not sane, one's been dl more than once")
 
 #############
 # Functions #
@@ -178,140 +188,10 @@ def add_to_list_of_values(dic: "Dict[A, List[B]]", key: "A", val: "B") -> None:
     else:
         l_elem.append(val)
 
-def download_puzzles_games(starting_line: int = 0) -> Dict[str, str]:
-    #Fields for the new db: PuzzleId,FEN,Moves,Rating,RatingDeviation,Popularity,NbPlays,Themes,GameUrl
-    with open(DB_PATH, newline='') as csvfile:
-        puzzles = csv.reader(csvfile, delimiter=',', quotechar='|')
-        chunk = 0 #games can be dl 500 at a time with a token
-        dep = time.time()
-        games = []
-        with open("puzzle_games.txt", "a") as output:
-            for puzzle in puzzles:
-                if chunk < starting_line: continue
-                if chunk % 300 == 0:
-                    res = req(games, chunk - 300, dep)
-                    output.write(res)
-                    games = []
-                game_id = puzzle[-1].split('/')[3].partition('#')[0] #ex: https://lichess.org/Fr7rv4jo/black#98 or https://lichess.org/JbCsF5hm#43
-                games.append(game_id)
-                
-                chunk += 1
-    return dic
-
-def update():
-    game_to_puzzle_id = game_puzzle_id()
-    l_games_dl = list_games_already_dl()
-    for game_id in l_games_dl:
-        if game_to_puzzle_id.pop(game_id, None) is None:
-            print(f"game {game_id} was dl but not in the puzzle db, Error")
-    # Only games in the db not dl are left in the dic
-    games_not_dl = list(game_to_puzzle_id.keys())
-    print(f"{len(games_not_dl)} games left to be dl, expecting {len(games_not_dl)/20:.2f}s")
-    c = 0
-    dep = time.time()
-    with open("puzzle_games.txt", "a") as output:
-        for i in range(0, len(games_not_dl), 300): #dl games 300 at a time
-            res = req(games_not_dl[i:i+300],c,dep)
-            output.write(res)
-            c += 300
-
-def list_games_already_dl() -> List[str]:
-    l = []
-    with open("puzzle_games.txt", "r") as file_input:
-        for line in file_input:
-            l.append(line.split()[0])
-    return l
-
-def game_puzzle_id() -> Dict[str, str]:
-    """
-    returns a dic game_id -> puzzle_id
-    A game can only produce at most one puzzle
-    """
-    dic = {}
-    #Fields for the new db: PuzzleId,FEN,Moves,Rating,RatingDeviation,Popularity,NbPlays,Themes,GameUrl
-    with open(DB_PATH, newline='') as csvfile:
-        puzzles = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for puzzle in puzzles:
-            game_id = puzzle[-1].split('/')[3].partition('#')[0]
-            dic[game_id] = puzzle[0]
-    return dic
-                
-def req(games: List[str], games_dl: int, dep: float) -> str:
-    res = ""
-    nb_processed = 0
-    current_game_id = ""
-    with open("raw_response.txt", "a") as raw_response:
-        raw_response.write("-"*20 + f"req requested at {time.time()}" + "-"*20 + "\n")
-        with requests.post("https://lichess.org/games/export/_ids?moves=false", data=",".join(games), stream=True) as r:
-            if r.status_code != 200:
-                print(f"\nError, http code: {r.status_code}")
-                time.sleep(65) #Respect rate-limits!
-            for line in r.iter_lines():
-                decoded_line = line.decode("utf-8")
-                raw_response.write(decoded_line + "\n")
-                m_game = GAME_ID_REGEX.match(decoded_line)
-                if m_game:
-                    current_game_id = m_game.group(1)
-
-                m_player = PLAYER_REGEX.match(decoded_line)
-                if m_player:
-                    if m_player.group(1) == "White":
-                        res += current_game_id + " " + m_player.group(2)
-                    else:
-                        res += " " + m_player.group(2) + "\n"
-                        print(f"\r{games_dl + nb_processed} games downloaded, {(time.time() - dep):.2f}s",end="")
-                        nb_processed += 1
-    return res
-
-def compute() -> List[Row]:
-    dic = {}
-    game_to_puzzle_id = game_puzzle_id()
-    with open("puzzle_games.txt", "r") as file_input:
-        for line in file_input:
-            args = line.split() # game id, white player, black player
-            for player in args[1:]: # first arg is game id
-                add_to_list_of_values(dic, player, game_to_puzzle_id[args[0]])
-    
-    # Sort the players
-    npp = lambda x: len(x[1]) # Number of Puzzles of the Player
-    sorted_tuple = sorted(dic.items(),key=npp,reverse=True)
-    l_row = []
-    rank = 1
-    nb_puzzles = npp(sorted_tuple[0])
-    # Add rank to each player
-    for row in sorted_tuple:
-        if npp(row) != nb_puzzles:
-            rank += 1
-            nb_puzzles = npp(row)
-        l_row.append(Row(rank=rank,player=row[0],l_puzzles=row[1]))
-    return l_row
-
-def save_csv(l_row: List[Row]) -> None:
-    with open("leaderboard.csv", "w") as csvfile:
-        fieldnames = ['rank', 'player', 'number_puzzles_generated', 'list_puzzle_ids']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for row in l_row:
-            writer.writerow({'rank': row.rank, 'player': row.player, 'number_puzzles_generated': len(row.l_puzzles), 'list_puzzle_ids':" ".join(row.l_puzzles)})
-
-def check_sanity():
-    """
-    Check if a game is present twice or more
-    """
-    s = set()
-    print("checking sanity of the games dl...")
-    with open("puzzle_games.txt", "r") as file_input:
-        for line in file_input:
-            game_id = line.split()[0]
-            if game_id in s:
-                print(game_id)
-            else:
-                s.add(game_id)
-
-
 def main():
     print(f"Creating leaderboard")
     file_handler = FileHandler()
+    file_handler.check_sanity()
     dler = Downloader(file_handler)
     dler.update()
     print(f"Computing")
@@ -326,7 +206,4 @@ def main():
 if __name__ == "__main__":
     print('#'*80)
     main()
-    #check_sanity()
-    #print("\n"+req(["gX1AOHfA", "9F9G6yiK", "zJy6vEgo"], 1, time.time()))
-    #bM5DTJxW OGNYN scarface2015
 
