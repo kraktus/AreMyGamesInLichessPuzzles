db20d50b066392d9a7ecf81898f58dfd7b0824b9
kraktus <56031107+kraktus@users.noreply.github.com>
2020-12-25 10:36:36 +0000

    Push fixed version of leaderboard

--- leaderboard.py
+++ leaderboard.py
@@ -69,6 +69,30 @@ def download_puzzles_games(starting_line: int = 0) -> Dict[str, str]:
                 chunk += 1
     return dic
 
+def update():
+    game_to_puzzle_id = game_puzzle_id()
+    l_games_dl = list_games_already_dl()
+    for game_id in l_games_dl:
+        if game_to_puzzle_id.pop(game_id, None) is None:
+            print(f"game {game_id} was dl but not in the puzzle db, Error")
+    # Only games in the db not dl are left in the dic
+    games_not_dl = list(game_to_puzzle_id.keys())
+    print(f"{len(games_not_dl)} games left to be dl, expecting {len(games_not_dl)/20:.2f}s")
+    c = 0
+    dep = time.time()
+    with open("puzzle_games.txt", "a") as output:
+        for i in range(0, len(games_not_dl), 300): #dl games 300 at a time
+            res = req(games_not_dl[i:i+300],c,dep)
+            output.write(res)
+            c += 300
+
+def list_games_already_dl() -> List[str]:
+    l = []
+    with open("puzzle_games.txt", "r") as file_input:
+        for line in file_input:
+            l.append(line.split()[0])
+    return l
+
 def game_puzzle_id() -> Dict[str, str]:
     """
     returns a dic game_id -> puzzle_id
@@ -82,13 +106,13 @@ def game_puzzle_id() -> Dict[str, str]:
             game_id = puzzle[-1].split('/')[3].partition('#')[0]
             dic[game_id] = puzzle[0]
     return dic
-
                 
 def req(games: List[str], games_dl: int, dep: float) -> str:
     res = ""
     nb_processed = 0
     current_game_id = ""
     with open("raw_response.txt", "a") as raw_response:
+        raw_response.write("-"*20 + f"req requested at {time.time()}" + "-"*20 + "\n")
         with requests.post("https://lichess.org/games/export/_ids?moves=false", data=",".join(games), stream=True) as r:
             if r.status_code != 200:
                 print(f"\nError, http code: {r.status_code}")
@@ -119,11 +143,13 @@ def compute() -> List[Row]:
             for player in args[1:]: # first arg is game id
                 add_to_list_of_values(dic, player, game_to_puzzle_id[args[0]])
     
+    # Sort the players
     npp = lambda x: len(x[1]) # Number of Puzzles of the Player
     sorted_tuple = sorted(dic.items(),key=npp,reverse=True)
     l_row = []
     rank = 1
     nb_puzzles = npp(sorted_tuple[0])
+    # Add rank to each player
     for row in sorted_tuple:
         if npp(row) != nb_puzzles:
             rank += 1
@@ -133,11 +159,11 @@ def compute() -> List[Row]:
 
 def save_csv(l_row: List[Row]) -> None:
     with open("leaderboard.csv", "w") as csvfile:
-        fieldnames = ['rank', 'player', 'number_puzzles_generated'] #, 'list_puzzle_ids']
+        fieldnames = ['rank', 'player', 'number_puzzles_generated', 'list_puzzle_ids']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for row in l_row:
-            writer.writerow({'rank': row.rank, 'player': row.player, 'number_puzzles_generated': len(row.l_puzzles)}) #, 'list_puzzle_ids':" ".join(row.l_puzzles)})
+            writer.writerow({'rank': row.rank, 'player': row.player, 'number_puzzles_generated': len(row.l_puzzles), 'list_puzzle_ids':" ".join(row.l_puzzles)})
 
 def check_sanity():
     """
@@ -160,6 +186,9 @@ def main():
         print("Game ids not stored, downloading...")
         download_puzzles_games()
         print("\nDownload finished")
+    else:
+        print("Game ids found, updating...")
+        update()
     print(f"Computing")
     dic = compute()
     print("saving to leaderboard.csv")
@@ -172,5 +201,7 @@ def main():
 if __name__ == "__main__":
     print('#'*80)
     main()
+    #check_sanity()
     #print("\n"+req(["gX1AOHfA", "9F9G6yiK", "zJy6vEgo"], 1, time.time()))
+    #bM5DTJxW OGNYN scarface2015
 
